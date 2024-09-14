af502096e964b11cb99c870deb82242f4fadffe5
Praj <prajmhatre2003@gmail.com>
2024-09-14 14:52:34 +0530

    Added the javascript function for image mapping

---
+++ script.js
@@ -0,0 +1,24 @@
+var name_stn = "";
+
+
+$(".stn").click(function (){
+
+    name_stn= $(this).attr("id");
+    console.log("global var: "+name_stn); //<-- for debugging
+})
+
+
+
+function setFrom() {
+    
+    $("#from").val(name_stn);
+    console.log("from var: "+name_stn);  //<-- for debugging
+  
+}
+
+function setTo() {
+   
+    $("#to").val(name_stn);
+    console.log("to var: "+name_stn);  //<-- for debugging
+   
+}
